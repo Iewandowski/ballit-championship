129397f07f8331c3b9d4e4d2c2103490d010f626
Carol Lewandowski <lewandowski.rod@gmail.com>
2024-08-06 00:48:23 -0300

    first commit

---
+++ backend-ballit-championship/src/main/java/com/lewandowski/backend_ballit_championship/BackendBallitChampionshipApplication.java
@@ -0,0 +1,13 @@
+package com.lewandowski.backend_ballit_championship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBallitChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBallitChampionshipApplication.class, args);
+	}
+
+}
---
+++ backend-ballit-championship/src/test/java/com/lewandowski/backend_ballit_championship/BackendBallitChampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewandowski.backend_ballit_championship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBallitChampionshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
